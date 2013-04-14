020e5f41de353947e61bac559c882d88d8aa3037
iceanfire <iceanfire@gmail.com>
2013-04-14 02:50:47 -0400

    misc updates

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ class Help(authHandler):
         # Send out the emails
 
         for user in userListing:
-            email.sendNotification(user.email(), 'LearnToDo Notification Update', ""+users.get_current_user().nickname()+" has posted a response to a thread you've signed up for. \n\r Click here to learn more: http://learntodo.co/feed#"+str(questionKey))
+            email.sendNotification(user.email(), 'LearnToDo Notification Update', ""+users.get_current_user().nickname()+" has posted a response to a thread you've signed up for. \r\n\r\nClick here to learn more: \r\nhttp://learntodo.co/feed#"+str(questionKey))
 
         self.redirect('/feed#'+str(questionKey))
 
